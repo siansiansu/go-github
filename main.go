5898596ff69b5af5efafeef75a8b03e8baae3326
alexsu <alexsu@17.media>
2021-12-27 22:41:56 +0800

    update main.go

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/google/go-github/github"
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
+)
+
+const (
+	owner       = "siansiansu"
+	repo        = "siansiansu"
+	contentPath = "README.md"
+	branch      = "main"
 )
 
 func main() {
@@ -15,9 +23,9 @@ func main() {
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("[FATA] error config file: %w \n", err))
+		panic(fmt.Errorf("Failed to read config file: %w \n", err))
 	}
-    token := viper.GetString("github_token")
+	token := viper.GetString("github_token")
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -25,8 +33,28 @@ func main() {
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
-	client := github.NewClient(tc)
+	c := github.NewClient(tc)
 
 	// Print Octocat
-	fmt.Println(client.Octocat(ctx, "hello world!"))
+	fmt.Println(c.Octocat(ctx, "hello world!"))
+
+	_, _, err = c.Repositories.List(ctx, "", nil)
+	if err != nil {
+		panic(fmt.Errorf("Failed to list repositories"))
+	}
+
+	// Get File From Github repositories
+	readCloser, err := c.Repositories.DownloadContents(ctx, owner, repo, contentPath, &github.RepositoryContentGetOptions{
+		Ref: branch,
+	})
+	defer readCloser.Close()
+
+	if err != nil {
+		panic(fmt.Errorf("Failed to get file"))
+	}
+	content, err := ioutil.ReadAll(readCloser)
+	if err != nil {
+		panic(fmt.Errorf("Failed to read file"))
+	}
+	fmt.Println(string(content))
 }
